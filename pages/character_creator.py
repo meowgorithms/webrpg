a1dc01a05baea1a30a64a2f8c8ef4d5ed01761ed
LoganTilley <logan.e.tilley@gmail.com>
2021-08-01 01:12:57 -0400

    init'd character_creator

---
+++ pages/character_creator.py
@@ -0,0 +1,14 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from app import db
+
+# Components
+# ----------
+
+column1 = dbc.Col([])
+
+
+# Layout
+layout = dbc.Row([column1])

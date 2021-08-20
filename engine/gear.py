91da50ecae0040499dc01d53829beb0f236ad211
LoganTilley <logan.e.tilley@gmail.com>
2021-08-20 04:32:46 -0400

    Weapons now level up. Time to figure out the combat system...

--- engine/gear.py
+++ engine/gear.py
@@ -1,21 +1,68 @@
 """
 Provides archetypes and engine with gear classes and functions
 """
-from dataclasses import dataclass, field
+import numpy as np
 from . import elements as el
 from . import name_provider as namer
-from random import choices, choice
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from engine import character
 
+RARITY_MULT = {
+    "common": 1,
+    "uncommon": 1.1,
+    "rare": 1.2,
+    "unique": 1.3,
+    "legendary": 1.5
+}
 
 class GearItemData:
+    stats_per_level = {
+        "max_health": 30,
+        "physical_attack": 10,
+        "physical_defense": 10,
+        "magic_attack": 10,
+        "magic_defense": 10,
+        "strength": 1,
+        "constitution": 1,
+        "intelligence": 1,
+        "light": 2,
+        "dark": 2,
+        "electric": 2,
+        "fire": 2,
+        "water":2,
+        "earth": 2,
+        "air": 2,
+        "metal": 2,
+        "acid": 2,
+        "psychic": 2,
+        "weird": 2
+    }
+    @property
+    def required_exp(self):
+        return round(10_000 / (1 + np.e ** (-.1 * (self.level - 25))))
+
+    # --- EXP ---
+    @property
+    def experience(self):
+        return self._experience
+
+    @experience.setter
+    def experience(self, value):
+        self._experience = value
+        while self._experience >= self.required_exp:
+            self._experience -= self.required_exp
+            self.level += 1
+            for stat in self.stats:
+                self.stats[stat] = round(self.stats[stat] \
+                    + self.stats_per_level[stat] * RARITY_MULT[self.rarity])
+
+
     def __init__(self, element: el.Element = None):
         self.name: str = ''
         self.level: int = 1
-        self.experience: int = 0
+        self._experience: int = 0
         self.requirements: dict = {}
         self.stats: dict = {}
         self.rarity: str = ''
@@ -27,12 +74,17 @@ class GearItemData:
 
 class GearItem(ABC):
     item_type: str
+    
     def __init__(self, element: el.Element = None):
         self.data = GearItemData(element)
+    
+    def give_exp(self, amount: int):
+        self.data.experience += amount
 
     def __repr__(self):
         return f"""
     {self.data.name}: Level {self.data.level} {self.__class__.__name__}
+    {self.data.experience} / {self.data.required_exp} EXP
     Rarity: {self.data.rarity.capitalize()}
     Element: {self.data.element.name.lower().capitalize()}
     Requirements:
@@ -77,8 +129,7 @@ class SpecialItem(GearItem):
     def __repr__(self):
         return super().__repr__()
 
-# This is really, really dumb
-# TODO Refactor this garbage
+
 class GearSet:
     """
     Container for equippables
